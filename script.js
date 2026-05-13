229f42ab61b6d2bbc334628cd796541726292521
jhonisilva <133233437+JhoniSlva@users.noreply.github.com>
2026-05-13 19:45:36 -0300

    Primeiro comit

---
+++ script.js
@@ -0,0 +1,32 @@
+const supabaseUrl = 'https://jhonieveronicacha.netlify.app/';
+const supabaseKey = 'https://tyeprfwdzshjunzqdwkg.supabase.co/rest/v1/';
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+async function salvarConvidado() {
+
+  const nome = document.getElementById('nome').value;
+
+  const presente = document.getElementById('presente').value;
+
+  const { data, error } = await supabase
+    .from('convidados')
+    .insert([
+      {
+        nome: nome,
+        presente: presente,
+        quantidade: 1,
+        confirmado: true
+      }
+    ]);
+
+  if(error){
+    alert('Erro!');
+    console.log(error);
+  } else {
+    alert('Presença confirmada!');
+  }
+
+}
