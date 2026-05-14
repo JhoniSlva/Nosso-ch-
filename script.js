5846f54c351dff6cca0dc847a940ff7ab137fcf8
jhonisilva <133233437+JhoniSlva@users.noreply.github.com>
2026-05-14 10:14:30 -0300

    att 2

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const entrarBtn = document.getElementById('entrarBtn');
 
+const flowers = document.querySelectorAll('.flower');
+
+document.addEventListener('mousemove',(e)=>{
+
+  const x = e.clientX / window.innerWidth;
+  const y = e.clientY / window.innerHeight;
+
+  flowers.forEach((flower,index)=>{
+
+    const speed = index === 0 ? 20 : -20;
+
+    flower.style.transform =
+    `translate(${x * speed}px, ${y * speed}px)`;
+
+  });
+
+});
+
 entrarBtn.addEventListener('click',()=>{
 
-  const nome = document.getElementById('nome').value;
+  const nome =
+  document.getElementById('nome').value;
 
-  if(nome.trim() === ''){
+  if(nome.trim() === '') return;
 
-    alert('Digite seu nome');
+  document.body.style.transition = '1s';
 
-    return;
-  }
+  document.body.style.opacity = '0';
 
-  alert(`Bem-vindo(a), ${nome} 💙`);
+  setTimeout(()=>{
+
+    window.location.href =
+    'presentes.html?nome=' + nome;
+
+  },1000);
 
 });
