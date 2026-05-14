901d2e5f6e49dc697b2a3de4a8d51a3231549d7b
jhonisilva <133233437+JhoniSlva@users.noreply.github.com>
2026-05-14 10:08:55 -0300

    att 1

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-const supabaseUrl = 'https://tyeprfwdzshjunzqdwkg.supabase.co';
+const entrarBtn = document.getElementById('entrarBtn');
 
-const supabaseKey = 'sb_publishable_XZdunX9uP8mhSfzdUKHaHg_Za692cri';
-
-const db = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
-
-async function salvarConvidado() {
+entrarBtn.addEventListener('click',()=>{
 
   const nome = document.getElementById('nome').value;
 
-  const presente = document.getElementById('presente').value;
+  if(nome.trim() === ''){
 
-  const { data, error } = await db
-    .from('convidados')
-    .insert([
-      {
-        nome: nome,
-        presente: presente,
-        quantidade: 1,
-        confirmado: true
-      }
-    ]);
+    alert('Digite seu nome');
 
-  if(error){
-    alert('Erro!');
-    console.log(error);
-  } else {
-    alert('Presença confirmada!');
+    return;
   }
 
-}
+  alert(`Bem-vindo(a), ${nome} 💙`);
+
+});
